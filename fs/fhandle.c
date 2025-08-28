af76643fdcc2a11ac7b000f5bedc4d8e415e32b7
Thomas Bertschinger <tahbertschinger@gmail.com>
2025-08-27 22:52:08 -0600

    fhandle: create do_file_handle_open() helper
    This pulls the code for opening a file, after its handle has been
    converted to a struct path, into a new helper function.
    
    This function will be used by io_uring once io_uring supports
    open_by_handle_at(2).
    
    Signed-off-by: Thomas Bertschinger <tahbertschinger@gmail.com>

--- fs/fhandle.c
+++ fs/fhandle.c
@@ -401,6 +401,20 @@ out_path:
 	return retval;
 }
 
+struct file *do_file_handle_open(struct path *path, int open_flag)
+{
+	const struct export_operations *eops;
+	struct file *file;
+
+	eops = path->mnt->mnt_sb->s_export_op;
+	if (eops->open)
+		file = eops->open(path, open_flag);
+	else
+		file = file_open_root(path, "", open_flag, 0);
+
+	return file;
+}
+
 static long do_handle_open(int mountdirfd, struct file_handle __user *ufh,
 			   int open_flag)
 {
@@ -408,7 +422,6 @@ static long do_handle_open(int mountdirfd, struct file_handle __user *ufh,
 	long retval = 0;
 	struct path path __free(path_put) = {};
 	struct file *file;
-	const struct export_operations *eops;
 
 	handle = get_user_handle(ufh);
 	if (IS_ERR(handle))
@@ -422,11 +435,7 @@ static long do_handle_open(int mountdirfd, struct file_handle __user *ufh,
 	if (fd < 0)
 		return fd;
 
-	eops = path.mnt->mnt_sb->s_export_op;
-	if (eops->open)
-		file = eops->open(&path, open_flag);
-	else
-		file = file_open_root(&path, "", open_flag, 0);
+	file = do_file_handle_open(&path, open_flag);
 	if (IS_ERR(file))
 		return PTR_ERR(file);
 
